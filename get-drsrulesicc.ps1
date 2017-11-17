ab63c79293eed4f278746a6c51f85c0f6c23d82d
BROUHARD Benjamin <brouhard@icc.unicc.org>
2017-11-17 17:25:05 +0100

    Add functions for export and list

--- get-drsrulesicc.ps1
+++ get-drsrulesicc.ps1
@@ -9,17 +9,45 @@
 .TAGS 
     VMware PowerCLI  
 .Dependency 
-DRSRules module
-https://github.com/PowerCLIGoodies/DRSRule/b
+    DRSRules module
+    https://github.com/PowerCLIGoodies/DRSRule/b
 #>
 
 
+##list all clusters and DRSrules
+Function get-alldrsrules {
+    foreach ($cluster in get-cluster) {
+        Write-Host "$cluster"
+        Get-DrsRule -Cluster $cluster
+        Get-DrsVMGroup -Cluster $cluster
+        Get-DrsVMToVMHostRule -Cluster $cluster
+        Get-DrsVMToVMRule -Cluster $cluster
+        Write-Host " "
+    } 
+}
 
-foreach ($cluster in get-cluster){
-   Write-Host "$cluster"
-    Get-DrsRule -Cluster $cluster
-    Get-DrsVMGroup -Cluster $cluster
-    Get-DrsVMToVMHostRule -Cluster $cluster
-    Get-DrsVMToVMRule -Cluster $cluster
-    Write-Host " "
-} 
+function export-alldrsrules {
+    foreach ($cluster in get-cluster) {
+        Export-DrsRule -Cluster $cluster -Path C:\temp\$cluster-export.json
+    }
+}
+
+$DRShostrulesquery = "select * from vm_drsrules
+where Ruletype = 'ClusterHostGroup'"
+
+$DRSHOSTGROUPS = BGMGT-querysql $DRShostrulesquery
+
+foreach ($HOSTGROUP in $DRSHOSTGROUPS ) { 
+    if (!(Get-DrsVMHostGroup -Name $HOSTGROUP.RuleName)) {
+        $DRShostmemberquery = "select member from vm_drsrules_members
+        where RuleName = '$($HOSTGROUP.RuleName)'"
+        $DRSHOSTMEMBERS = BGMGT-querysql $DRShostmemberquery
+            foreach ($MEMBERS in $DRSHOSTMEMBERS){
+                $MEMBER += $MEMBERS.Member
+            }
+
+        New-DrsVMHostGroup -Name $HOSTGROUP.RuleName  -VMHost $MEMBER -Cluster $HOSTGROUP.cluster
+    }
+}
+
+#export-alldrsrules
