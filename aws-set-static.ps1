ab313059c589cb7d003ae43bf5b029815844a4cc
benbjamin24 <bbrouhard@hotmail.com>
2020-09-02 20:40:33 +0200

    Change DHCP address to static.

---
+++ aws-set-static.ps1
@@ -0,0 +1,25 @@
+﻿
+
+
+$wkspIP =  Get-NetIPConfiguration |where {$_.netProfile.name -eq "SEC.UNJSPF.ORG"}
+
+$adapter = $wkspIP.InterfaceAlias
+Get-NetIPConfiguration |where {$_.netProfile.name -eq "SEC.UNJSPF.ORG"}
+#Disable-NetAdapter -Name $adapter -Confirm:$false
+
+#enable-NetAdapter -Name $adapter -Confirm:$false
+
+
+
+Remove-NetIPAddress -InterfaceAlias $wkspIP.InterfaceAlias -Confirm:$false
+
+$wkspIP.IPv4Address.IPAddress
+$wkspIP.IPv4DefaultGateway.nexthop
+
+$adapter | New-NetIPAddress `
+ -AddressFamily "IPv4" `
+ -IPAddress $wkspIP.IPv4Address.IPAddress `
+ -PrefixLength "24" `
+ -DefaultGateway $wkspIP.IPv4DefaultGateway.nexthop
+
+ Get-NetIPConfiguration -InterfaceAlias $adapter
