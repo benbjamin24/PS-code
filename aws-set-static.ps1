d9527f113874bc118a6bda44a73dc54aa96eef2c
benbjamin24 <bbrouhard@hotmail.com>
2020-09-02 21:14:40 +0200

    Update aws-set-static.ps1

--- aws-set-static.ps1
+++ aws-set-static.ps1
@@ -3,26 +3,23 @@
 
 $profilename = "SEC.UNJSPF.ORG"
 
-
-
-
 #get Current IP address
 $wkspIP =  Get-NetIPConfiguration |where {$_.netProfile.name -eq $profilename}
 $wkspIP
 $adapter = $wkspIP.InterfaceAlias
 
-#Disable-NetAdapter -Name $adapter -Confirm:$false
 
+#Disable-NetAdapter -Name $adapter -Confirm:$false
 #enable-NetAdapter -Name $adapter -Confirm:$false
 
-
+start-sleep -Seconds 20
 
 Remove-NetIPAddress -InterfaceAlias $wkspIP.InterfaceAlias -Confirm:$false
 
 $wkspIP.IPv4Address.IPAddress
 $wkspIP.IPv4DefaultGateway.nexthop
 
-$adapter | New-NetIPAddress `
+Get-NetAdapter -Name $adapter | New-NetIPAddress `
  -AddressFamily "IPv4" `
  -IPAddress $wkspIP.IPv4Address.IPAddress `
  -PrefixLength "24" `
